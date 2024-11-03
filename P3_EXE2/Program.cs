64e8e57bfe46aab600bd3155da876e7f847d1f1c
Cesar18-hub <a32980@alunos.ipca.pt>
2024-11-03 12:09:21 +0000

    P3_EXE2

---
+++ P3_EXE2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace P3_EXE2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o valor do pH da solução: ");
+
+            // converteo para double
+            if (double.TryParse(Console.ReadLine(), out double ph))
+            {
+                if (ph < 7)
+                {
+                    Console.WriteLine("A solução é ácida.");
+                }
+                else if (ph == 7)
+                {
+                    Console.WriteLine("A solução é neutra.");
+                }
+                else
+                {
+                    Console.WriteLine("A solução é alcalina.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Valor de pH inválido. Por favor, insira um número.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
