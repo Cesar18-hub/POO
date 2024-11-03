ab1c3f49568ca22c99bbe152458002154de5c940
Cesar18-hub <a32980@alunos.ipca.pt>
2024-11-03 11:52:02 +0000

    P3_EXE1

---
+++ P3_EXE1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P3_EXE1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            string nome1 = "";
+            string nome2 = "";
+            int idade1 = 0;
+            int idade2 = 0;
+
+            Console.Write("Primeira pessoa: ");
+            Console.Write("\nNome completo: ");
+            nome1 = Console.ReadLine();
+            Console.Write("Idade: ");
+            idade1 = int.Parse(Console.ReadLine());
+
+            Console.Write("\nSegunda pessoa: ");
+            Console.Write("\nNome completo: ");
+            nome2 = Console.ReadLine();
+            Console.Write("Idade: ");
+            idade2 = int.Parse(Console.ReadLine());
+
+            if (idade1 > idade2) 
+            {
+                Console.WriteLine($"\nA pessoa mais velho/a é {nome1}");
+            }
+            else 
+            {
+                Console.WriteLine($"\nA pessoa mais velho/a é {nome2}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
